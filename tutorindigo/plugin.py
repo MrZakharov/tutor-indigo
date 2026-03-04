392f49f0d972b38f16bb413a702785b7a1191221
MrZakharov <andrew@zakharov.moscow>
2026-03-04 13:39:32 +0300

    fix

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -129,9 +129,9 @@ for mfe in indigo_styled_mfes:
                 f"mfe-dockerfile-post-npm-install-{mfe}",
                 f"""
 ENV {generate_secure_random_string(10)}={generate_secure_random_string(10)}
-RUN rm -rf node_modules/@edx/brand package-lock.json
-RUN npm install '@edx/brand@github:@MrZakharov/brand-openedx#indigo-2.5.1'
+#RUN rm -rf node_modules/@edx/brand package-lock.json
 RUN npm cache clean --force
+RUN npm install @edx/brand@github:@MrZakharov/brand-openedx#indigo-2.5.1
 """,  # noqa: E501
             ),
         ]
@@ -142,9 +142,9 @@ hooks.Filters.ENV_PATCHES.add_item(
         "mfe-dockerfile-post-npm-install-authn",
         f"""
 ENV {generate_secure_random_string(10)}={generate_secure_random_string(10)}        
-RUN rm -rf node_modules/@edx/brand package-lock.json
-RUN npm install '@edx/brand@github:@MrZakharov/brand-openedx#indigo-2.5.1'
+#RUN rm -rf node_modules/@edx/brand package-lock.json
 RUN npm cache clean --force
+RUN npm install @edx/brand@github:@MrZakharov/brand-openedx#indigo-2.5.1
 """,
     )
 )
